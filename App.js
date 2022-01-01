47a3611ac4da5912c0c9077b776be489f92abc71
kumankoishi.py <7iggif@naver.com>
2022-01-02 00:27:02 +0900

    Applied structure from components

--- App.js
+++ App.js
@@ -1,58 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import {
   StyleSheet,
-  Text,
-  View,
-  ScrollView,
-  Pressable,
-  TextInput
+  View
 } from 'react-native';
 
 import { store } from './store/Store';
 import { Provider } from 'react-redux';
 
+import ChatRoomTitle from './components/ChatRoomTitle';
+import ChatList from './components/ChatList';
+import ChatManager from './components/ChatManager';
+import SenderSelector from './components/SenderSelector';
+
 export default function App() {
   return (
     <Provider store={store}>
       <View style={styles.container}>
-        <View style={styles.chatRoomTitle}>
-          <Text>Chat Room Title</Text>
-        </View>
-
-        <ScrollView style={styles.chatList}>
-          <View style={styles.yourMessage}>
-            <Text>Your Message</Text>
-          </View>
-          <View style={styles.opponentMessage}>
-            <Text>The Opponent's Message</Text>
-          </View>
-        </ScrollView>
-
-        <View style={styles.chatManager}>
-          <Pressable style={styles.modeButton}>
-            <Text>Update Mode</Text>
-          </Pressable>
-          <Pressable style={styles.modeButton}>
-            <Text>Delete Mode</Text>
-          </Pressable>
-          <View style={styles.chatInput}>
-            <TextInput
-              placeholder='Enter a chat.'
-            />
-            <Pressable>
-              <Text>Send</Text>
-            </Pressable>
-          </View>
-        </View>
-
-        <View style={styles.senderSelector}>
-          <Pressable>
-            <Text>Opponent</Text>
-          </Pressable>
-          <Pressable>
-            <Text>You</Text>
-          </Pressable>
-        </View>
+        <ChatRoomTitle />
+        <ChatList />
+        <ChatManager />
+        <SenderSelector />
 
         <StatusBar style="auto" />
       </View>
@@ -66,29 +33,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  chatRoomTitle: {
-
-  },
-  chatList: {
-
-  },
-  yourMessage: {
-
-  },
-  opponentMessage: {
-
-  },
-  chatManager: {
-
-  },
-  modeButton: {
-
-  },
-  chatInput: {
-
-  },
-  senderSelector: {
-
   }
 });
