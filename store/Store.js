537e09b50122cdd3776b0c42a73373b0f9328ea7
kumankoishi.py <7iggif@naver.com>
2022-01-03 11:50:16 +0900

    Declared store variables and functions

--- store/Store.js
+++ store/Store.js
@@ -1,4 +1,5 @@
 import { createStore } from 'redux';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 /*
   chatRoomTitle: string
@@ -13,7 +14,68 @@ import { createStore } from 'redux';
   currentSender: 'you' | 'opponent'
 */
 
-function onDispatch(state = {}, action) {
+// Keys
+const TITLE_KEY = '@title';
+const YOUR_NAME_KEY = '@yourName';
+const OPPONENT_NAME_KEY = '@opponentName';
+const MESSAGE_LIST_KEY = '@messages';
+
+// Init state
+async function getInitialState() {
+  try {
+    // Get title
+    const rawTitle = await AsyncStorage.getItem(TITLE_KEY);
+    const title = (rawTitle !== null) ? rawTitle : 'Chat';
+
+    // Get your name
+    const rawYourName = await AsyncStorage.getItem(YOUR_NAME_KEY);
+    const yourName = (rawYourName !== null) ? rawYourName : 'You';
+
+    // Get opponent name
+    const rawOpponentName = await AsyncStorage.getItem(OPPONENT_NAME_KEY);
+    const opponentName = (rawOpponentName !== null) ? rawOpponentName : 'You';
+
+    // Get chat list
+    const rawMessageList = await AsyncStorage.getItem(MESSAGE_LIST_KEY);
+    const messages = (rawMessageList !== null) ? JSON.parse(rawMessageList) : {};
+
+    return {
+      title,
+      yourName,
+      opponentName,
+      messages,
+      currentMode: 'CREATE',
+      currentSender: 'you'
+    };
+  } catch (e) {
+    console.log('An error occured loading from storage');
+  }
+}
+
+const initState = await getInitialState();
+
+// Functions on dispatch
+function createMessage(state, newMessage) {
+  // TODO: Implement creating message
+}
+
+function updateMessage(state, key, newMessage) {
+  // TODO: Implement updating message
+}
+
+function deleteMessage(state, key) {
+  // TODO: Implement deleting message
+}
+
+function changeMode(state, newMode) {
+  // TODO: Implement changing mode
+}
+
+function changeSender(state, newSender) {
+  // TODO: Implement changing sender
+}
+
+function onDispatch(state = initState, action) {
   switch (action.type) {
     case 'CREATE-MESSAGE':
       break;
@@ -26,7 +88,7 @@ function onDispatch(state = {}, action) {
     case 'CHANGE-SENDER':
       break;
     default:
-      return action;
+      return state;
   }
 }
 
