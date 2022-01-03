b1deefd8552f5f9d97d97fd88c1d23ebc0b4d7af
kumankoishi.py <7iggif@naver.com>
2022-01-04 07:54:00 +0900

    Write and send Chats

--- components/ChatManager.js
+++ components/ChatManager.js
@@ -6,8 +6,38 @@ import {
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { FontAwesome } from '@expo/vector-icons';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 export default function ChatManager() {
+  const [status, setStatus] = useState({
+    chatInput: ''
+  });
+
+  const dispatch = useDispatch();
+
+  const onChangeTextInput = (payload) => {
+    setStatus({
+      ...status,
+      chatInput: payload
+    });
+  };
+
+  const onSendChat = () => {
+    dispatch({
+      type: 'CREATE-MESSAGE',
+      payload: {
+        newMessage: {
+          message: status.chatInput
+        }
+      }
+    });
+    setStatus({
+      ...status,
+      chatInput: ''
+    });
+  };
+
   return (
     <View style={styles.chatManager}>
       <Pressable style={styles.button}>
@@ -19,8 +49,15 @@ export default function ChatManager() {
       <TextInput
         style={styles.chatInput}
         placeholder="Enter a chat."
+        returnKeyType="send"
+        value={status.chatInput}
+        onChangeText={onChangeTextInput}
+        onSubmitEditing={onSendChat}
       />
-      <Pressable style={styles.button}>
+      <Pressable
+        style={styles.button}
+        onPress={onSendChat}
+      >
         <FontAwesome name="send-o" size={36} color="black" />
       </Pressable>
     </View>
@@ -39,6 +76,7 @@ const styles = StyleSheet.create({
     marginVertical: 8
   },
   chatInput: {
-    flex: 1
+    flex: 1,
+    fontSize: 18
   }
 });
