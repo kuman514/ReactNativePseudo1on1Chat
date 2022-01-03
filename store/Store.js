853caf9afff8248c14d8eacb343031937cf5a503
kumankoishi.py <7iggif@naver.com>
2022-01-03 14:48:06 +0900

    Splitted store functions to small modules

--- store/Store.js
+++ store/Store.js
@@ -1,92 +1,37 @@
 import { createStore } from 'redux';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
-/*
-  chatRoomTitle: string
-  messages: {
-    [Date.now()]: {
-      sender: 'you' | 'opponent',
-      timestamp: new Date(),
-      message: string
-    }, ...
-  }
-  currentMode: 'CREATE' | 'UPDATE' | 'DELETE'
-  currentSender: 'you' | 'opponent'
-*/
-
-// Keys
-const TITLE_KEY = '@title';
-const YOUR_NAME_KEY = '@yourName';
-const OPPONENT_NAME_KEY = '@opponentName';
-const MESSAGE_LIST_KEY = '@messages';
-
-// Init state
-async function getInitialState() {
-  try {
-    // Get title
-    const rawTitle = await AsyncStorage.getItem(TITLE_KEY);
-    const title = (rawTitle !== null) ? rawTitle : 'Chat';
-
-    // Get your name
-    const rawYourName = await AsyncStorage.getItem(YOUR_NAME_KEY);
-    const yourName = (rawYourName !== null) ? rawYourName : 'You';
-
-    // Get opponent name
-    const rawOpponentName = await AsyncStorage.getItem(OPPONENT_NAME_KEY);
-    const opponentName = (rawOpponentName !== null) ? rawOpponentName : 'You';
-
-    // Get chat list
-    const rawMessageList = await AsyncStorage.getItem(MESSAGE_LIST_KEY);
-    const messages = (rawMessageList !== null) ? JSON.parse(rawMessageList) : {};
-
-    return {
-      title,
-      yourName,
-      opponentName,
-      messages,
-      currentMode: 'CREATE',
-      currentSender: 'you'
-    };
-  } catch (e) {
-    console.log('An error occured loading from storage');
-  }
-}
-
-const initState = await getInitialState();
-
-// Functions on dispatch
-function createMessage(state, newMessage) {
-  // TODO: Implement creating message
-}
-
-function updateMessage(state, key, newMessage) {
-  // TODO: Implement updating message
-}
-
-function deleteMessage(state, key) {
-  // TODO: Implement deleting message
-}
-
-function changeMode(state, newMode) {
-  // TODO: Implement changing mode
-}
-
-function changeSender(state, newSender) {
-  // TODO: Implement changing sender
-}
+import {
+  initState
+} from './State';
+import {
+  createMessage,
+  updateMessage,
+  deleteMessage,
+  changeMode,
+  changeSender,
+  changeTitle,
+  changeYourName,
+  changeOpponentName
+} from './DispatchFunctions';
 
 function onDispatch(state = initState, action) {
   switch (action.type) {
     case 'CREATE-MESSAGE':
-      break;
+      return createMessage(state, action.payload.newMessage);
     case 'UPDATE-MESSAGE':
-      break;
+      return updateMessage(state, action.payload.newMessage);
     case 'DELETE-MESSAGE':
-      break;
+      return deleteMessage(state, action.payload.key);
     case 'CHANGE-MODE':
-      break;
+      return changeMode(state, action.payload.newMode);
     case 'CHANGE-SENDER':
-      break;
+      return changeSender(state, action.payload.newSender);
+    case 'CHANGE-TITLE':
+      return changeTitle(state, action.payload.newTitle);
+    case 'CHANGE-YOUR-NAME':
+      return changeYourName(state, action.payload.newName);
+    case 'CHANGE-OPPONENT-NAME':
+      return changeOpponentName(state, action.payload.newName);
     default:
       return state;
   }
