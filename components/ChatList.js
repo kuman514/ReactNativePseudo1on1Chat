ac6bba022ef9714153a5b070cafe00440f68ebb9
kumankoishi.py <7iggif@naver.com>
2022-01-04 07:53:25 +0900

    Render ChatList

--- components/ChatList.js
+++ components/ChatList.js
@@ -1,61 +1,31 @@
 import {
   StyleSheet,
-  Text,
   View,
   ScrollView
 } from 'react-native';
+import { useSelector } from 'react-redux';
+
+import Chat from './Chat';
 
 export default function ChatList() {
-  const tmpYourStyle = true;
+  const chatIdSelector = (state) => {
+    return Object.keys(state.messages);
+  };
+  const chatIds = useSelector(chatIdSelector);
 
   return (
     <ScrollView style={styles.chatList}>
       <View style={styles.chatCommon}>
-        <View style={
-          tmpYourStyle ? styles.yourChat : styles.opponentChat
-        }>
-          <Text style={styles.name}>
-            You
-          </Text>
-        </View>
-        
-        <View style={
-          tmpYourStyle ? styles.yourChat : styles.opponentChat
-        }>
-          <View style={{
-            ...styles.message,
-            ...(tmpYourStyle ? styles.yourMessage : styles.opponentMessage)
-          }}>
-            <Text style={styles.messageText}>
-              Your Message
-            </Text>
-          </View>
-          <View style={styles.timestamp}>
-            <Text>Timestamp</Text>
-          </View>
-        </View>
-      </View>
-
-      <View style={styles.chatCommon}>
-        <View style={styles.opponentChat}>
-          <Text style={styles.name}>
-            The Opponent
-          </Text>
-        </View>
-        
-        <View style={styles.opponentChat}>
-          <View style={{
-            ...styles.message,
-            ...(!tmpYourStyle ? styles.yourMessage : styles.opponentMessage)
-          }}>
-            <Text style={styles.messageText}>
-              The Opponent's Message
-            </Text>
-          </View>
-          <View style={styles.timestamp}>
-            <Text>Timestamp</Text>
-          </View>
-        </View>
+        {
+          chatIds.map((chatId) => {
+            return (
+              <Chat
+                key={chatId}
+                id={chatId}
+              />
+            );
+          })
+        }
       </View>
     </ScrollView>
   );
@@ -64,36 +34,5 @@ export default function ChatList() {
 const styles = StyleSheet.create({
   chatList: {
     alignSelf: 'stretch'
-  },
-  chatCommon: {
-    marginHorizontal: 10,
-    marginVertical: 8
-  },
-  name: {
-    fontSize: 18,
-    fontWeight: '700',
-    marginBottom: 5
-  },
-  message: {
-    padding: 10,
-    borderRadius: 5
-  },
-  messageText: {
-    fontSize: 20
-  },
-  timestamp: {
-    flexDirection: 'column-reverse'
-  },
-  yourChat: {
-    flexDirection: 'row-reverse'
-  },
-  yourMessage: {
-    backgroundColor: 'yellow'
-  },
-  opponentChat: {
-    flexDirection: 'row'
-  },
-  opponentMessage: {
-    backgroundColor: 'white'
   }
 });
