1561315d644866ff5ca25529320e9d7b2de710cf
kumankoishi.py <7iggif@naver.com>
2022-01-09 15:51:12 +0900

    Added loading title

--- components/ChatRoomTitle.js
+++ components/ChatRoomTitle.js
@@ -1,14 +1,31 @@
+import { useState } from 'react';
 import {
   StyleSheet,
   Text,
   View
 } from 'react-native';
+import { useSelector } from 'react-redux';
 
 export default function ChatRoomTitle() {
+  const chatTitleSelector = (state) => {
+    return state.title;
+  };
+  const chatTitle = useSelector(chatTitleSelector);
+
+  const [status, setStatus] = useState({
+    editTitle: chatTitle
+  });
+
+  // Get mode for rendering edit
+  const modeSelector = (state) => {
+    return state.currentMode;
+  };
+  const currentMode = useSelector(modeSelector);
+
   return (
     <View style={styles.chatRoomTitle}>
       <Text style={styles.titleFont}>
-        Chat Room Title
+        { chatTitle }
       </Text>
     </View>
   );
@@ -18,7 +35,8 @@ const styles = StyleSheet.create({
   chatRoomTitle: {
     backgroundColor: 'brown',
     paddingHorizontal: 20,
-    paddingVertical: 20,
+    paddingTop: 30,
+    paddingBottom: 20,
     flexDirection: 'row',
     alignSelf: 'stretch'
   },
