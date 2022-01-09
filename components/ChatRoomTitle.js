0b1d45c2d1b13260e9ca14d51724f84d154278fa
kumankoishi.py <7iggif@naver.com>
2022-01-09 16:06:12 +0900

    Implemented editing title

--- components/ChatRoomTitle.js
+++ components/ChatRoomTitle.js
@@ -1,20 +1,47 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput
 } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function ChatRoomTitle() {
+  const dispatch = useDispatch();
+
   const chatTitleSelector = (state) => {
     return state.title;
   };
   const chatTitle = useSelector(chatTitleSelector);
 
+  // Declare title text for editing
   const [status, setStatus] = useState({
     editTitle: chatTitle
   });
+
+  // Update functions
+  const onChangeText = (payload) => {
+    setStatus({
+      editTitle: payload
+    });
+  };
+  const updateTitle = (changedTitle) => {
+    dispatch({
+      type: 'CHANGE-TITLE',
+      payload: {
+        newTitle: changedTitle
+      }
+    });
+    setStatus({
+      editTitle: changedTitle
+    });
+  };
+  const resetText = () => {
+    setStatus({
+      editTitle: chatTitle
+    });
+  };
 
   // Get mode for rendering edit
   const modeSelector = (state) => {
@@ -22,11 +49,33 @@ export default function ChatRoomTitle() {
   };
   const currentMode = useSelector(modeSelector);
 
+  useEffect(() => {
+    if (currentMode !== 'UPDATE' && status.editTitle !== chatTitle) {
+      resetText();
+    }
+  });
+
   return (
     <View style={styles.chatRoomTitle}>
-      <Text style={styles.titleFont}>
-        { chatTitle }
-      </Text>
+      {
+        (currentMode === 'UPDATE') ? (
+          <TextInput
+            style={{
+              ...styles.titleFont,
+              ...styles.editTitle
+            }}
+            value={status.editTitle}
+            onChangeText={onChangeText}
+            onSubmitEditing={() => {
+              updateTitle(status.editTitle);
+            }}
+          />
+        ) : (
+          <Text style={styles.titleFont}>
+            { chatTitle }
+          </Text>
+        )
+      }
     </View>
   );
 }
@@ -43,5 +92,8 @@ const styles = StyleSheet.create({
   titleFont: {
     fontSize: 24,
     color: 'white'
+  },
+  editTitle: {
+    textDecorationLine: 'underline'
   }
 });
